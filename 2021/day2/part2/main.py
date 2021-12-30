5f986cdc239151b3963311ea58f1027c9d4e430d
Mahmoud Fathi <m.fathi1991@gmail.com>
2021-12-30 21:10:27 +0100

    Add the solution for day2 part2.

---
+++ 2021/day2/part2/main.py
@@ -0,0 +1,21 @@
+import os
+
+input_file = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'input')
+
+with open(input_file) as lines:
+    horizontal_position = 0
+    vertical_position = 0
+    aim = 0
+    for line in lines:
+        line_elements = line.split()
+        command = line_elements[0]
+        command_value = int(line_elements[1])
+        if command == 'forward':
+            horizontal_position += command_value
+            vertical_position += (command_value * aim)
+        elif command == 'up':
+            aim -= command_value
+        elif command == 'down':
+            aim += command_value
+
+    print(horizontal_position * vertical_position)    # 1857958050
