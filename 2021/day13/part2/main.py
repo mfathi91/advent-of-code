f1092af013be881a7926a921d4309a2b8df55517
Mahmoud Fathi <m.fathi1991@gmail.com>
2022-01-16 16:42:16 +0100

    Add the solution for day13 part2.

---
+++ 2021/day13/part2/main.py
@@ -0,0 +1,74 @@
+import os
+import re
+from typing import Tuple, Set, List
+
+
+def parse_paper(lines: List[str]) -> Set[Tuple]:
+    paper = set()
+    for line in lines:
+        if not line:
+            break
+        x = int(line.split(',')[0])
+        y = int(line.split(',')[1])
+        paper.add((x, y))
+    return paper
+
+
+def parse_fold_instructions(lines: List[str]) -> List[Tuple]:
+    fold_instructions = []
+    fold_instructions_start = False
+    for line in lines:
+        if fold_instructions_start:
+            instruction = re.findall('[x|y]=[0-9]+', line)[0]
+            xy, value = instruction.split('=')[0], int(instruction.split('=')[1])
+            fold_instructions.append((xy, value))
+        if not line:
+            fold_instructions_start = True
+
+    return fold_instructions
+
+
+def fold(paper: Set[Tuple], fold_command: str, fold_from: int) -> Set[Tuple]:
+    folded_paper = set()
+    for dot in paper:
+        x, y = dot[0], dot[1]
+        if fold_command == 'x':
+            if x >= fold_from:
+                folded_paper.add((2 * fold_from - x, y))
+            else:
+                folded_paper.add((x, y))
+
+        elif fold_command == 'y':
+            if y >= fold_from:
+                folded_paper.add((x, 2 * fold_from - y))
+            else:
+                folded_paper.add((x, y))
+    return folded_paper
+
+
+def draw_paper(paper: Set[Tuple]):
+    max_x = 0
+    max_y = 0
+    for dot in paper:
+        max_x, max_y = max(max_x, dot[0]), max(max_y, dot[1])
+
+    for i in range(max_y + 1):
+        line = ''
+        for j in range(max_x + 1):
+            line += '#' if (j, i) in paper else '.'
+        print(line)
+
+
+def main():
+    input_file = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'input')
+    with open(input_file) as f:
+        lines = [line.rstrip() for line in f]
+        paper = parse_paper(lines)
+        for fold_instruction in parse_fold_instructions(lines):
+            paper = fold(paper, fold_instruction[0], fold_instruction[1])
+
+        draw_paper(paper)    # CJCKBAPB
+
+
+if __name__ == '__main__':
+    main()
